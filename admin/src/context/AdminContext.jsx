a331b70c58fc53a408f4ffba254106e8598a9e65
MdArhanUrRehman <arhanurrehman12@gmail.com>
2025-08-09 17:00:40 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://appointo-backend-fn6i.onrender.com/"
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
 
