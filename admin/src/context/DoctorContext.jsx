3dc0704ec9e6c36eb223905c1a2960eab117ecb8
MdArhanUrRehman <arhanurrehman12@gmail.com>
2025-08-09 17:01:38 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://appointo-backend-fn6i.onrender.com/";
 
   const [appointments, setAppointments] = useState([]);
   const [profileData, setProfileData] = useState(false);
