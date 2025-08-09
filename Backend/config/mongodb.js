e787a72589f5150dc93290c038c79d629306bf20
MdArhanUrRehman <arhanurrehman12@gmail.com>
2025-08-09 18:29:59 +0530

    Update mongodb.js

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true,
+  useUnifiedTopology: true});
     console.log("mongoDB connected");
   } catch (error) {
     console.log(error);
